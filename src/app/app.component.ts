4daaececa218024226ce0e1e56b83464b7ab481c
nilesh230123 <123529379+nilesh230123@users.noreply.github.com>
2023-02-07 21:36:34 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { products} from '../products';
-import { customers } from '../customersData';
+import { products} from './products';
+import { customers } from './customersData';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { customers } from '../customersData';
 })
 export class AppComponent {
 products=products;
-customers-customers;
+customers=customers;
 }
 
 
