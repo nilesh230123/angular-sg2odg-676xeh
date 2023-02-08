7b2e7c2e5d8fc6215dd377877c51fbd48249f9e3
nilesh230123 <123529379+nilesh230123@users.noreply.github.com>
2023-02-08 12:59:17 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { products} from './products';
-import { customers } from './customersData';
+// import { Component } from '@angular/core';
+// import { products} from './products';
+// import { customers } from './customersData';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-products=products;
-customers=customers;
-}
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+// products=products;
+// customers=customers;
+// }
 
 
