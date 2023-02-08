456ff2f27eee5c0f482610ed2c6d0e300fe8290e
nilesh230123 <123529379+nilesh230123@users.noreply.github.com>
2023-02-08 10:11:02 +0530

    1 file added

---
+++ src/app/app.OfferComponent.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { products} from './products';
+import { customers } from './customersData';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+products=products;
+customers=customers;
+}
